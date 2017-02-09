2c38482ccc2c461ac75b88e5b92546323eda232a
Shane Mileham <shane.mileham@gmail.com>
2017-02-09 13:10:35 -0800

    Wrote calculateInfluence to satisfy test cases

--- test/calculateInfluence.test.js
+++ test/calculateInfluence.test.js
@@ -4,11 +4,11 @@ describe('calculateInfluence', () => {
   let calculateInfluence = require('../lib/calculateInfluence.js');
 
   it('should return a zero-filled array when given no inputs', () => {
-    let influence = calculateInfluence();
+    let influence = calculateInfluence([]);
     influence.filter((element) => element !== 0).should.be.empty;
   });
 
-  it.skip('should calculate the influence of food lasting two hours', () => {
+  it('should calculate the influence of food lasting two hours', () => {
     let input = [{
       'type': 'food',
       'index': 60,
@@ -19,7 +19,7 @@ describe('calculateInfluence', () => {
     influence.filter((element) => element !== 0).length.should.equal(2*60);
   });
 
-  it.skip('should calculate the influence of exercise lasting an hour', () => {
+  it('should calculate the influence of exercise lasting an hour', () => {
     let input = [{
       'type': 'exercise',
       'index': 60,
@@ -30,7 +30,7 @@ describe('calculateInfluence', () => {
     influence.filter((element) => element !== 0).length.should.equal(60);
   });
 
-  it.skip('should calculate the influence of food totalling its GI', () => {
+  it('should calculate the influence of food totalling its GI', () => {
     let input = [{
       'type': 'food',
       'index': 60,
@@ -42,7 +42,7 @@ describe('calculateInfluence', () => {
     sum.should.equal(60);
   });
 
-  it.skip('should calculate the influence of exercise totalling its EI', () => {
+  it('should calculate the influence of exercise totalling its EI', () => {
     let input = [{
       'type': 'exercise',
       'index': 60,
@@ -54,5 +54,28 @@ describe('calculateInfluence', () => {
     sum.should.equal(-60);
   });
 
-  it('should work for an example input');
+  it('should work for overlapping food and exercise', () => {
+    let input = [
+      {
+        'type': 'food',
+        'index': 120,
+        'time': 0,
+      },
+      {
+        'type': 'exercise',
+        'index': 60,
+        'time': 30,
+      },
+    ];
+
+    let influence = calculateInfluence(input);
+    let sum = influence.reduce((a, b) => a + b, 0);
+    sum.should.equal(60);
+
+    // Array should look like [1,1,...,0,0,...,1,1,...0,0...]
+    influence.slice(0, 30).filter((element) => element !== 1).should.be.empty;
+    influence.slice(30, 90).filter((element) => element !== 0).should.be.empty;
+    influence.slice(90, 120).filter((element) => element !== 1).should.be.empty;
+    influence.slice(120).filter((element) => element !== 0).should.be.empty;
+  });
 });
