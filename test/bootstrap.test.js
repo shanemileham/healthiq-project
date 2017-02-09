aabddc4bfdca8249e289a118d063b6cac32c592a
Shane Mileham <shane.mileham@gmail.com>
2017-02-09 09:32:10 -0800

    Tested that readInputs should read a valid input correctly

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -1,3 +1,4 @@
 module.exports = sinon = require('sinon');
+module.exports = assert = require('chai').assert;
 module.exports = expect = require('chai').expect;
 module.exports = should = require('chai').should();
--- test/readInputs.test.js
+++ test/readInputs.test.js
@@ -6,10 +6,31 @@ describe('readInputs', () => {
   it('should return a rejected promise if the input is undefined', () => {
     readInputs(undefined)
     .then(() => {
-      assert.fail('words');
+      assert.fail('readInputs should reject undefined input');
     })
     .catch((error) => {
       error.should.equal('Input is undefined');
     });
   });
+
+  it('should read a valid input correctly', sinon.test(() => {
+    let data = `[
+      {
+        "type": "exercise",
+        "id": 3,
+        "time": "420"
+      }
+    ]`;
+    let fsp = require('fs-promise');
+    let fspStub = sinon.stub(fsp, 'readFile');
+    fspStub.returns(Promise.resolve(data));
+
+    readInputs('exampleInput.json')
+    .then((output) => {
+      output.toString().should.equal(JSON.parse(data).toString());
+    })
+    .catch((error) => {
+      assert.fail('Error reading valid input');
+    });
+  }));
 });
