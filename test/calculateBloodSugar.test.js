e77b14977f716d352e48857da8176a6120b3489d
Shane Mileham <shane.mileham@gmail.com>
2017-02-10 11:59:03 -0800

    Working on tests

--- test/calculateBloodSugar.test.js
+++ test/calculateBloodSugar.test.js
@@ -13,7 +13,26 @@ describe('calculateBloodSugar', () => {
     calculateBloodSugar(array)[0].should.equal(80);
   });
 
-  it('should correctly calculate blood sugar under influence (nonzero)');
-  it('should correctly calculate blood sugar with zero influence');
+  it('should correctly calculate blood sugar under influence (nonzero)', () => {
+    let array = new Array(60).fill(1);
+    let bloodSugar = calculateBloodSugar(array);
+    bloodSugar.forEach((element, index) => {
+      element.should.equal(80 + index);
+    });
+  });
+
+  it('should correctly calculate blood sugar with zero influence', () => {
+    let array = new Array(60).fill(0);
+    array[0] = 1;
+    let bloodSugar = calculateBloodSugar(array);
+    bloodSugar.forEach((element, index) => {
+      if (index === 0) {
+        element.should.equal(80);
+      } else {
+        element.should.equal(81);
+      }
+    });
+  });
+
   it('should correctly calculate blood sugar with undefined influence');
 });
